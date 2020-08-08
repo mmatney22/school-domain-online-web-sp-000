0807681a42d07a8db14f1eba54bc1c89bb19851d
Morgan Matney <morgan.matney89@gmail.com>
2020-08-08 14:12:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,16 +7,17 @@ class School
   end
 
   def add_student(name, grade)
-    @roster[grade] ||= []
+    roster[grade] ||= []
+    roster[grade] << name
   end
 
   def grade(grade)
-    @roster[grade]
+    roster[grade]
   end
 
   def sort
     sorted = {}
-    @roster.each do |grade, names|
+    roster.each do |grade, names|
       sorted[grade] = names.sort
     end
     sorted
